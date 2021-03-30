0684fed7395149bbdea3a759dd24c78327ee96e7
jemblonganvalley <jemblonganvalley@gmail.com>
2021-03-31 01:15:23 +0700

    update

--- public/js/main.js
+++ public/js/main.js
@@ -39,3 +39,45 @@ const handleChange = (event) => {
   lv_resepsi_lokasi.innerHTML = data_lv.lokasi;
   lv_pesan.innerHTML = data_lv.pesan;
 };
+
+// fungsi untuk merubah font pada class lv_mempelai
+const ubahFont = (event) => {
+  //tangkap component class lv_mempelai
+  let lv_mempelai = document.querySelector(".lv_mempelai");
+
+  //ubah font sesuai seleksi
+  lv_mempelai.style.fontFamily = event.target.value;
+};
+
+const ubahFontSize = (event) => {
+  //tangkap component class lv_mempelai
+  let lv_mempelai = document.querySelector(".lv_mempelai");
+
+  //ubah font sesuai seleksi
+  lv_mempelai.style.fontSize = event.target.value + "px";
+};
+
+const getPexelImage = () => {
+  let mib = document.querySelector(".modal_image_body");
+
+  fetch("https://api.pexels.com/v1/search?query=background&per_page=27", {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "563492ad6f9170000100000182dd3a28a29a4c6d8e0d9264be150a09",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.photos.map((e) => {
+        mib.innerHTML += `
+          <img src="${e.src.tiny}" width="32.5%"/>
+        `;
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+getPexelImage();
